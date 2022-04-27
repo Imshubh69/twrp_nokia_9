bce3cf4866e80186e746424d375ae33c1e38c452
Shubh <vishwajitg471@gmail.com>
2022-04-27 08:00:10 +0530

    sprout: twrp-ify
    Signed-off-by: Shubh <vishwajitg471@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_AOP_sprout.mk
+    $(LOCAL_DIR)/twrp_AOP_sprout.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_AOP_sprout-user \
-    omni_AOP_sprout-userdebug \
-    omni_AOP_sprout-eng
+    twrp_AOP_sprout-user \
+    twrp_AOP_sprout-userdebug \
+    twrp_AOP_sprout-eng
--- twrp_AOP_sprout.mk
+++ twrp_AOP_sprout.mk
@@ -13,13 +13,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Inherit from AOP_sprout device
 $(call inherit-product, device/hmd/AOP_sprout/device.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/twrp/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := AOP_sprout
-PRODUCT_NAME := omni_AOP_sprout
+PRODUCT_NAME := twrp_AOP_sprout
 PRODUCT_BRAND := Nokia
 PRODUCT_MODEL := Nokia 9
 PRODUCT_MANUFACTURER := hmd
